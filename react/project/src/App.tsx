9016e11863cd8ebd4db2492cc7cddd4c7a75ec25
Aritana Noara <arinoara@yahoo.com.br>
2022-03-23 23:20:58 -0300

    componentization

--- react/project/src/App.tsx
+++ react/project/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from './components/form';
+import List from './components/list';
 
 
 function App() {
   return (
     <div className="App">
-          <h1> Hello World</h1>
+      <Form />
+      <List></List>
     </div>
   );
 }
