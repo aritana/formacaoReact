bb1b96f1e5914c601844cfb4bbbab7342e37b039
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2022-04-10 06:45:50 -0300

    starting in vue

---
+++ vue/tracker/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <main class="columns is-gapless is-multiline">
+
+    <div class="column is-1">
+      <barra-lateral />
+    </div>
+
+    <div class="column is-11">
+   <formulario/>
+    </div>
+
+  </main>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from './components/MeuFormulario.vue';
+
+
+
+export default defineComponent({
+  components: { BarraLateral, Formulario },
+  name: "App",
+});
+</script>
+
+<style>
+</style>
---
+++ vue/tracker/src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/etracker.png" alt="logotipo da pagina" />
+    </h1>
+  </header>
+</template>
+
+
+<!--exportar componente-->
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BarraLateral",
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+/* mobile */
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
